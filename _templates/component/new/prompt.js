f1d08da040c0e33be6a718c42bf74d2f1814578f
reymon359 <ramonmorcillocascales@gmail.com>
2020-10-19 17:33:03 +0100

    feat: add prompt js

---
+++ _templates/component/new/prompt.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    type: 'input',
+    name: 'path',
+    message: 'New component path: (empty for \'components/\')'
+  },
+  {
+    type: 'input',
+    name: 'name',
+    message: 'New component name:'
+  }
+]
